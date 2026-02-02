ab9a50ec21f321a30f8e340861f6f8f820f8f7a8
Muhammad Faraz <mfarazzzz@gmail.com>
2026-02-02 13:05:21 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,37 +1,36 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      sizeLimit: env.int('UPLOAD_SIZE_LIMIT', 50 * 1024 * 1024),
-    },
-  },
-  i18n: {
-    enabled: true,
-    config: {
-      locales: ['hi', 'en'],
-      defaultLocale: 'hi',
-    },
-  },
-  'auto-slug-manager': {
-    enabled: true,
-  },
-  'strapi-advanced-sitemap': {
-    enabled: true,
-  },
-  'users-permissions': {
-    enabled: true,
-    config: {
-      jwt: {
-        expiresIn: '7d',
-      },
-      register: {
-        allowedFields: ['username', 'email', 'password'],
-      },
-      advancedSettings: {
-        unique_email: true,
-        allow_register: true,
-        email_confirmation: false,
-        default_role: 'authenticated',
-      },
-    },
-  },
-});
+export default ({ env }) => ({ 
+   upload: { 
+     config: { 
+       sizeLimit: env.int('UPLOAD_SIZE_LIMIT', 50 * 1024 * 1024), 
+     }, 
+   }, 
+   i18n: { 
+     enabled: true, 
+     config: { 
+       locales: ['hi', 'en'], 
+       defaultLocale: 'hi', 
+     }, 
+   }, 
+   'auto-slug-manager': { 
+     enabled: true, 
+   }, 
+   'strapi-advanced-sitemap': { 
+     enabled: true, 
+   }, 
+   'users-permissions': { 
+     enabled: true, 
+     config: { 
+       jwt: { 
+         expiresIn: '7d', 
+       }, 
+       register: { 
+         allowedFields: ['username', 'email', 'password'], 
+       }, 
+       advancedSettings: { 
+         unique_email: true, 
+         allow_register: true,  email_confirmation: false, 
+         default_role: 'authenticated', 
+       }, 
+     }, 
+   }, 
+ });
